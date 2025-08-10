c7c978ab4d43c2a04b4d993474550877a5fa6230
Knlight47 <90705399+Knlight47@users.noreply.github.com>
2025-08-10 18:36:12 +0700

    Add files via upload

---
+++ lib/supabase.ts
@@ -0,0 +1,13 @@
+import { createBrowserClient, createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+export const supabaseBrowser = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+export const supabaseServer = () =>
+  createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies }
+  );
