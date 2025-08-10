e358bdc565bc1a4a308e57cf1362fdea0746fdc4
Knlight47 <90705399+Knlight47@users.noreply.github.com>
2025-08-11 02:07:38 +0700

    Update supabase-admin.ts

--- lib/supabase-admin.ts
+++ lib/supabase-admin.ts
@@ -1,9 +1,8 @@
-// lib/supabase-admin.ts
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = () =>
   createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!, // ใช้ service role
-    { auth: { persistSession: false } }
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,          // URL เดิม
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,         // ใช้ Service Role Key
+    { auth: { autoRefreshToken: false, persistSession: false } }
   );
